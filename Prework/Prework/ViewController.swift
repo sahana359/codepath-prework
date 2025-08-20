cb1488f68cb72b8a16050cf89ed6b758d1a69c06
Sahana Rajashekara <sahana359@gmail.com>
2025-08-20 17:27:17 +0530

    Add Prework folder

---
+++ Prework/Prework/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Sahana Rajashekara on 8/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.7)
+            }
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+    }
+}
+
